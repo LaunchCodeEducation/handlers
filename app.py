99eee56d6732d6ddcb9f8004734a1ca25340d570
Chris Bay <chris@chrisbay.net>
2019-05-22 18:26:56 -0500

    Prep for deployment

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+TEMPLATE = """
+<!doctype html>
+<html>
+    <head>
+        <title>Request Parrot</title>
+    </head>
+    <body>
+        <h1>Request Parrot</h1>
+        <p>Request type: {req_type}</p>
+        <p>Request parameters:</p>
+        <ul>
+            {params}
+        </ul>
+    </body>
+</html>
+"""
+
+@app.route("/request-parrot", methods=['POST', 'GET'])
+def print_form_values():
+    params = ""
+    for field in request.form.keys():
+        params += "<li><b>{key}</b>: {value}</li>".format(key=field, value=request.form[field])
+
+    return TEMPLATE.format(req_type = request.method,
+                params=params)
+
+if __name__ == "__main__":
+    app.run()
